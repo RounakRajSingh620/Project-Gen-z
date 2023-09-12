13ba1f7f43aea4eed678d157de31720894c58e50
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-09-13 01:24:57 +0530

    GenZ

--- gen-z/src/components/Login.jsx
+++ gen-z/src/components/Login.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import './Login.css'
 function Login() {
   return (
-    <div className='box'> 
-      <div className="text-wrapper">Login</div>
+    <div className='box'>
+      <div className="label">
+        <div className="text-wrapper">
+          Login
+        </div>
+      </div>
+      <div className="values">
+
+        <Form>
+          <Form.Group className="email" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email Id</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Form.Group>
+          <Form className="password">
+            <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+            <Form.Control
+              type="password"
+              id="inputPassword5"
+              aria-describedby="passwordHelpBlock"
+            />
+          <Button className='button' variant="secondary" size="lg">
+           Login
+          </Button>
+          </Form>
+        </Form>
+      </div>
     </div>
-    
+
+
+
+
+
   )
 }
 
