6b341f32dd868820e4eb9c07ef65a2f4d62bad26
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-09-12 22:30:20 +0530

    Genz

--- gen-z/src/App.jsx
+++ gen-z/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
 import Hero from './components/Hero'
-
 function App() {
   
   return (
     <>
+   <Login/>
     <Hero></Hero>
 
     </>
---
+++ gen-z/src/components/Login.jsx
@@ -0,0 +1,11 @@
+imp from 'react'
+
+function Login() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
