7e993f84f13dada7cfd1ac1bbc3b25431e525ab9
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-09-12 18:23:16 +0530

    Project Gen-Z

---
+++ gen-z/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Hero from './components/Hero'
+
+function App() {
+  
+  return (
+    <>
+    <Hero></Hero>
+
+    </>
+  )
+}
+
+export default App
