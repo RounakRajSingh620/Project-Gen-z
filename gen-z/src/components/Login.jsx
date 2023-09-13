e2b5438d644140a53e201562f23081a1f41b222a
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-09-13 12:03:42 +0530

    GenZ

--- gen-z/src/components/Login.jsx
+++ gen-z/src/components/Login.jsx
@@ -5,6 +5,7 @@ import './Login.css';
 import google from "../assets/google.png";
 import facebook from "../assets/facebook.png";
 import apple from "../assets/apple.png";
+import girlimage from "../assets/girlimage.png";
 
 
 function Login() {
@@ -15,39 +16,36 @@ function Login() {
           Login
         </div>
       </div>
-      <div className="values">
-        <Form>
-          <Form.Group className="email" controlId="exampleForm.ControlInput1">
-            <Form.Label>Email Id</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
-          </Form.Group>
-          <Form className="password">
-            <Form.Label htmlFor="inputPassword5">Password</Form.Label>
-            <Form.Control
-              type="password"
-              id="inputPassword5"
-              aria-describedby="passwordHelpBlock"
-            />
-            <Button className='button' variant="secondary" size="lg">
-              Login
-            </Button>
-            <div className="signUp">
-              <div className="line"></div>
-              <div className="sign">
-                Or Sign Up with
-              </div>
-              <div className="line"></div>
-            </div>
-            <div className="images">
+      <Form.Group className="email" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email Id</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+        <Form.Control
+          type="password"
+          id="inputPassword5"
+          aria-describedby="passwordHelpBlock"/>
+      </Form.Group>
+      <Button className='button' variant="secondary" size="lg">
+        Login
+      </Button>
+      <div className="signUp">
+        <div className="line"></div>
+        <div className="sign">
+          Or Sign Up with
+        </div>
+        <div className="line"></div>
+      </div>
+      <div className="images">
 
-              <img  src={google} alt="google" />
-              <img src={facebook} alt="facebook" />
-              <img src={apple} alt="apple" />
-            </div>
-          </Form>
-        </Form>
+        <img src={google} alt="google" />
+        <img src={facebook} alt="facebook" />
+        <img src={apple} alt="apple" />
+      </div>
+      <div className="girlimage">
+        <img src={girlimage} alt='girl image' />
       </div>
     </div>
+
   );
 }
 
